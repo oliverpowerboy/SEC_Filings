cba32ec5acd7ab1023b3c6979a57f07ca21454f8
oliverpowerboy <oliverd@live.co.uk>
2022-12-27 13:47:25 +0000

    first

--- read data.py
+++ read data.py
@@ -8,26 +8,32 @@ import warnings
 matches = 0
 scanned_lines = 0
 
-
+# Main function
 def main():
-    # submissions_data = extract_tsv("Sec filings\\2022_05_notes.zip", "sub.tsv")
-    # numeric_data = extract_tsv()
 
+
+    # only used in filt_form_type_and_company but here for clairty
     company = "APPLE INC"
 
+    # generate list of paths for the zip files
     zip_files = [{"path": os.path.join(root, file), "name": file} for root, dirs, files in os.walk("Sec filings") for
                  file in files]
 
+    # submissions pandas data frame
+    # process_submissions() returns a data frame each time so contact is used to combine the data frames
     sub_data = pd.concat([process_submissions(zip["path"]) for zip in zip_files])
 
     filt_form_type_and_company = (
-                (sub_data["name"] == "APPLE INC") & ((sub_data["form"] == "10-K") | (sub_data["form"] == "10-Q")))
+                (sub_data["name"] == company) & ((sub_data["form"] == "10-K") | (sub_data["form"] == "10-Q")))
 
+    # remove all none related entries and reset the index
     sub_data = sub_data.loc[filt_form_type_and_company]
     sub_data.reset_index(inplace=True)
 
+    # Numeric data is a massive data frame so process_numbers() handles all of the work
     num_data = [process_numbers(zip["path"], sub_data["adsh"]) for zip in zip_files]
 
+    # create data frame and save the data as it takes a while to process the data
     try:
         pd.concat(num_data).to_csv("AccountsReceivableNetCurrent APPL.csv")
     except:
@@ -42,12 +48,12 @@ def main():
 
 def process_submissions(path):
     try:
-
+        # Open and read each sub.tsv file as a data frame
         with zipfile.ZipFile(path, "r") as working_directory:
             with working_directory.open("sub.tsv") as sub:
                 return pd.read_csv(sub, sep="\t")
 
-
+    #Zip file failed does nothing and gives a warning
     except zipfile.BadZipfile:
         warnings.warn(f"bad zip : {path} ")
 
@@ -58,26 +64,36 @@ def process_numbers(path, adsh, cunksize=1_000_000):
 
     try:
 
+        #Open and read num.tsv in chunks
         with zipfile.ZipFile(path, "r") as working_directory:
             with working_directory.open("num.tsv") as sub:
                 use_cols = ["adsh", "tag", "value", "ddate", "qtrs"]
 
+                # chunk processing
+                # Helps save on current memory used, doesn't do much for speed performance
+
                 for chunk in pd.read_csv(sub, sep="\t", chunksize=cunksize, usecols=use_cols):
+
+                    # Tracks how many lines have been scanned so far
                     scanned_lines += chunk.shape[0]
 
+                    # adsh is passed in from when it is called
+                    # adsh is a filter for documents, since we filtered the submissions earlier for the company it effectively filters for company
                     adsh_filter = chunk["adsh"].isin(adsh)
-
                     chunk = chunk[adsh_filter]
 
+                    # Now we filter by tag
                     tag_filter = chunk["tag"] == "AccountsReceivableNetCurrent"
+                    chunk = chunk[tag_filter]
 
-                    matches += chunk[tag_filter].shape[0]
-
+                    # tracks how many matches and print progress
+                    matches += chunk.shape[0]
                     print(f"found : {matches:,}\n{str(round((scanned_lines / 281_165_204) * 100, 2))}%")
 
+                    # convert ddate column to datetime object
                     chunk['ddate'] = pd.to_datetime(chunk['ddate'], format="%Y%m%d")
 
-                    return chunk[tag_filter]
+                    return chunk
 
 
     except zipfile.BadZipfile:
